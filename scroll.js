8ff5df1dfc6acc1007cadc4a10ee8fe76d99d99e
Nathan Woodburn <contact@nathan.woodburn.id.au>
2022-09-19 22:34:46 +1000

    Added back to top button

--- scroll.js
+++ scroll.js
@@ -1,11 +1,18 @@
+// Add an observer to check when divs are visible on screen
+
+
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
+    // For each div with the hidden tag
+    entries.forEach((entries) => {
+        // if the div is on screen
+        if (entries.isIntersecting) {
+            // Add the show tag which animates it on screen
+            entries.target.classList.add('show');
         }
     });
 });
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+// Get hidden divs
+const hiddenDivs = document.querySelectorAll('.hidden');
+// For each hidden div, check if it is visible
+hiddenDivs.forEach((el) => observer.observe(el));
---
+++ totop.js
@@ -0,0 +1,32 @@
+// Select To Top Div
+var totop = document.getElementById('totop');
+
+// On click scroll to top of page
+totop.onclick = function() {
+    window.scrollTo(0, 0);
+};
+
+// Make button only show when page scrolled down a bit
+
+// Whenever the window scrolls run the function
+window.onscroll = function() {scrollFunction()};
+
+// When scroll
+function scrollFunction() {
+
+    // if the scroll is at least 500 pixel from the top
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        // Show the To Top button
+
+        // Transition delay needs to be there to stop the visibility messing with the fade
+        totop.style.transitionDelay = "0ms"
+        totop.style.visibility = "visible";
+        totop.style.opacity = "1";
+    } else {
+        // Hide the To Top button when the page scroll is near the top
+
+        totop.style.transitionDelay = ""
+        totop.style.visibility = "hidden";
+        totop.style.opacity = "0";
+    }
+}
