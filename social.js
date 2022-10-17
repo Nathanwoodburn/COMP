c5306c8aa7250d532ee1a30aa7ec5a82345f52ab
Nathan Woodburn <github@nathan.woodburn.au>
2022-10-17 15:34:55 +1100

    Added social feed

---
+++ social.js
@@ -0,0 +1,7 @@
+// document.getElementById('socialframe').contentWindow.scrollTo(10,100);
+
+function scrollframe(frame) {
+    console.log("loaded");
+// frame.contentWindow.scrollTo(100);
+document.getElementById('socialframe').contentWindow.scrollTo(100);
+}
